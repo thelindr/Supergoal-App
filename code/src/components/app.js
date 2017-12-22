5bbc2334d836b4f31b21a145c0e6ef06ad44db34
Sofia Boström <sofia.bostrom@aftonbladet.se>
2017-12-22 16:32:54 +0100

    Checkboxes completed

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,8 +1,8 @@
 import React from "react"
-import { BrowserRouter, Route } from "react-router-dom"
-import Mainscreen from "./mainscreen"
+// import { BrowserRouter, Route } from "react-router-dom"
+// import Mainscreen from "./mainscreen"
 
-import SetSuperGoal from "./setSuperGoal"
+// import SetSuperGoal from "./setSuperGoal"
 import SetTask from "./setTask"
 import ListItem from "./listItem"
 
@@ -19,20 +19,32 @@ export default class App extends React.Component {
     }
   }
 
-  updateSuperGoal = (newSuperGoalName, newSuperGoalValue) => {
-    console.log("parent function was invoked")
-    this.setState({
-      superGoal: {
-        name: newSuperGoalName,
-        value: newSuperGoalValue
-      }
-    })
-  }
+  // updateSuperGoal = (newSuperGoalName, newSuperGoalValue) => {
+  //   console.log("parent function was invoked")
+  //   this.setState({
+  //     superGoal: {
+  //       name: newSuperGoalName,
+  //       value: newSuperGoalValue
+  //     }
+  //   })
+  // }
 
   addTaskToList = taskName => {
     console.log("Task updated in parent", taskName)
     this.setState({
       taskList: [...this.state.taskList, { name: taskName, done: false }]
+    })
+  }
+
+  updateDoneStatus = nameOfTaskUpdate => {
+    const updatedListOfTasks = this.state.taskList.map(item => {
+      if (item.name === nameOfTaskUpdate) {
+        item.done = !item.done
+      }
+      return item
+    })
+    this.setState({
+      taskList: updatedListOfTasks
     })
   }
 
@@ -44,9 +56,11 @@ export default class App extends React.Component {
         <SetTask
           addTaskToList={this.addTaskToList} />
         <h1>My tasks:</h1>
-        {this.state.taskList.map(item => {
-          return <ListItem />
-        })}
+        {this.state.taskList.map(item => (
+          <ListItem
+            item={item}
+            updateDone={this.updateDoneStatus} />
+        ))}
       </div>
       // <BrowserRouter>
       //   <div>
--- code/src/components/listItem/index.js
+++ code/src/components/listItem/index.js
@@ -2,10 +2,18 @@ import React from "react"
 
 export default class ListItem extends React.Component {
 
+  handleCheckboxChange = () => {
+    this.props.updateDone(this.props.item.name)
+  }
+
   render() {
     return (
       <div>
-        This is the Main Screen! WOHOO!
+        <input
+          type="checkbox"
+          checked={this.props.item.done}
+          onChange={this.handleCheckboxChange} />
+        {this.props.item.name}
       </div>
     )
   }

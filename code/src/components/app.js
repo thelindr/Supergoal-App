f0205739416115f76cd982a5daee01027fe13d82
kmkkkuk <kmkkuk@gmail.com>
2017-12-22 14:59:28 +0100

    router added

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { BrowserRouter, Route } from "react-router-dom"
+import Mainscreen from "./mainscreen"
 
 import SetSuperGoal from "./setSuperGoal"
 
@@ -27,10 +29,13 @@ export default class App extends React.Component {
   render() {
     console.log("Thank you, mom got your supergoal:", this.state.superGoal)
     return (
-      <div>
-        <SetSuperGoal
-          updateSuperGoalInApp={this.updateSuperGoal} />
-      </div>
+      <BrowserRouter>
+        <div>
+          {/* <SetSuperGoal
+            updateSuperGoalInApp={this.updateSuperGoal} /> */}
+          <Route path="/" exact render={() => <SetSuperGoal updateSuperGoalInApp={this.updateSuperGoal} />} />
+        </div>
+      </BrowserRouter>
     )
   }
 

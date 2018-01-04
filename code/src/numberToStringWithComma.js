3485fd72e87274f4d1ba2acf9755dce13548673e
Fredrik <osterlund.f@gmail.com>
2018-01-04 17:07:03 +0100

    improved numberToStringWithComma (two decimals when needed)

--- code/src/numberToStringWithComma.js
+++ code/src/numberToStringWithComma.js
@@ -1,3 +1,7 @@
-export default number => (
-  number.toString().replace(".", ",")
-)
+export default number => {
+  if (number % 1 !== 0) {
+    return number.toFixed(2).toString().replace(".", ",")
+  } else {
+    return number.toString().replace(".", ",")
+  }
+}
